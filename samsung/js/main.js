89269b15cbfaa752a68e2d4166821daac09486bc
hyeinji0704 <157083486+hyeinji0704@users.noreply.github.com>
2024-02-02 13:42:20 +0900

    .

---
+++ samsung/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	console.log('hello i feel faint')
+    /* 브라우저를 스크롤했을 때
+    맨위면 header에 fixed라는 클래스를 삭제
+    조금이라도 아래로 스크롤된 상태라면
+    header에 fixed라는 클래스를 줌 
+    1. 브라우저가 스크롤 된 정도를 표시한 값
+        $(window).scrollTop()
+    2. 만약 스크롤 값이 0이라면
+        header에 fixed라는 클래스를 삭제
+        만약 스크롤 값이 0보다 크다면 
+        header에 fixed라는 클래스
+    */
+    $(window).scroll(function(){
+        let scrolling = $(window).scrollTop()
+        //console.log(scrolling)
+        //조건문 - scrolling값이 0보다 크면 
+        if(scrolling > 0){
+            //console.log('0보다크다')
+            $(".header").addClass("fixed")
+        }else{ // 0일 때 - 맨 위에 있을 때 
+            //console.log('0이다')
+            $(".header").removeClass("fixed")
+        }
+    })//$(window).scroll
+})//$(document).ready

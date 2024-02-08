2083682669f13693349bfe5e9f8c9a82d12de9c2
hyeinji0704 <157083486+hyeinji0704@users.noreply.github.com>
2024-02-08 18:12:01 +0900

    .

--- samsung_menu/js/main.js
+++ samsung_menu/js/main.js
@@ -67,4 +67,20 @@ $(document).ready(function(){
     //  .header .gnb ul.depth01 > li > a
     // 1차메뉴 a를 클릭하면 감싸는 li에 클래스를 추가
     // a는 href 이동값을 가지고 있음... 이게 모바일에서는 클릭되면 안됩니다!
+
+    // top버튼을 클릭하면 상단으로 자동 스크롤
+    $('.footer .top').on('click', function(){
+        $('html,body').animate({
+            scrollTop : 0
+        }, 400)
+    })
+    const swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+	autoplay: {  /* 팝업 자동 실행 */
+		delay: 3000,
+		disableOnInteraction: true,
+	},
+
+	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+});
 })//$(document).ready

55e8e04327259b80fdc064f2692aa5ed57339853
hyeinji0704 <157083486+hyeinji0704@users.noreply.github.com>
2024-03-28 18:13:06 +0900

    .

--- hospital/js/common.js
+++ hospital/js/common.js
@@ -4,3 +4,85 @@
     작성일 : 2024.03.28
     속  성 : 모든 페이지에서 작동되는 jquery (header, footer에서 작동되는 jquery)
 */
+$(document).ready(function(){
+    /*******************브라우저가 스크롤이 되면 header fixed 클래스 추가****************/
+    let scrolling 
+    function scroll_chk(){
+        scrolling = $(window).scrollTop()
+        if(scrolling > 0){//조금이라도 스크롤 함
+            $('.header').addClass('fixed')
+        }else{//상단에 있는 경우 -- 스크롤 안한 경우 또는 다시 올라간 경우
+            $('.header').removeClass('fixed')
+        }
+    }
+    scroll_chk() //문서 로드되었을 때 1번 실행
+    $(window).scroll(function(){
+        scroll_chk()
+    })
+
+    
+    /*****************메뉴 오버 and 오픈**************** */
+    let device_status
+    let window_w
+    function device_chk(){
+        let window_w = $(window).width()
+        if(window_w > 1260 ){ //pc
+            device_status = 'pc'
+        }else{ //모바일
+            device_status = 'mobile'
+        }
+        console.log(device_status)
+    }
+    device_chk()//문서가 로딩되고 1번 실행
+    $(window).resize(function(){ //문서가 리사이즈될 때마다 1번씩 실행
+        device_chk()
+    })
+
+    
+    $('.header .gnb .depth01 > li').on('mouseenter focusin', function(){
+       if(device_status == 'pc') {
+            $('.header').addClass('menu_over')
+            $('.header .gnb .depth01 > li').removeClass('on')
+            $(this).addClass('on')
+        }
+    })
+    $('.header').on('mouseleave', function(){
+        if(device_status == 'pc') {
+            $('.header').removeClass('menu_over')
+            $('.header .gnb .depth01 > li').removeClass('on')
+        }
+    })
+    $('.header .family').on('focusin', function(){
+        if(device_status == 'pc') {
+            $('.header').removeClass('menu_over')
+            $('.header .gnb .depth01 > li').removeClass('on')
+        }
+    })
+    $('.header .share').on('click', function(){
+        $(this).toggleClass('on')
+        $('html').on('click', function(e){
+            if($(e.target).parents('.header .share').length < 1){
+                $('.header .share').removeClass('on')
+            }
+        })
+    })
+    $('.header .gnb .depth01 > li > a').on('click', function(e){
+        if(device_status == 'mobile') {
+            e.preventDefault()
+            $(this).parent().toggleClass('on')
+        }
+    })
+    $('.header .gnb .gnb_open').on('click', function(){
+        if(device_status == 'mobile') {
+            $('.header').addClass('menu_open')
+            $("html, body").css({overflow : "hidden", height : $(window).height()}).bind("scroll touchmove mousewheel", function(e){e.preventDefault();e.stopPropagation();return false;},function(){passive:false});
+        }
+    })
+    $('.header .gnb .gnb_close').on('click', function(){
+        if(device_status == 'mobile') {
+            $('.header').removeClass('menu_open')
+            $("html, body").css({overflow : "visible", height : "auto"}).unbind('scroll touchmove mousewheel');
+        }
+    })
+    /*****************메뉴 오버 and 오픈**************** */
+})
--- hospital/js/main.js
+++ hospital/js/main.js
@@ -4,3 +4,44 @@
     작성일 : 2024.03.28
     속  성 : 메인페이지에서 사용된 jqeury(header 빼고)
 */
+$(document).ready(function(){
+
+
+        const swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+            effect: "fade", /* fade 효과 */
+
+            autoplay: {  /* 팝업 자동 실행 */
+                delay: 3000,
+                disableOnInteraction: true,
+            },
+
+            loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+
+            pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+                el: '.visual .pagination', /* 해당 요소의 class명 */
+                clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+                /* type: 'fraction',  type fraction을 주면 paging이 숫자로 표시됨 */
+                renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
+                    return '<span class="' + className + '">' + (index + 1) + "</span>";
+                },
+            },
+
+            navigation: {  /* 이전, 다음 버튼 */
+                nextEl: '.visual .next',  /* 다음 버튼의 클래스명 */
+                prevEl: '.visual .prev',  
+            },
+
+        });
+        
+        $('.visual .swiper .pause').on('click', function(){
+            swiper.autoplay.stop(); /* 일시정지 기능 */
+            $('.visual .swiper .play').show()
+            $('.visual .swiper .pause').hide()
+        })
+        $('.visual .swiper .play').on('click', function(){
+            swiper.autoplay.start();  /* 재생 기능 */
+            $('.visual .swiper .pause').show()
+            $('.visual .swiper .play').hide()
+        })
+})

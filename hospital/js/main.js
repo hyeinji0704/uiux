61d78ed143ddf595f82ac7b17e516e610b97964e
hyeinji0704 <157083486+hyeinji0704@users.noreply.github.com>
2024-03-29 18:03:03 +0900

    .

--- hospital/js/main.js
+++ hospital/js/main.js
@@ -5,7 +5,21 @@
     속  성 : 메인페이지에서 사용된 jqeury(header 빼고)
 */
 $(document).ready(function(){
-
+        let device_status
+        let window_w
+        function device_chk(){
+            let window_w = $(window).width()
+            if(window_w > 1260 ){ //pc
+                device_status = 'pc'
+            }else{ //모바일
+                device_status = 'mobile'
+            }
+            console.log(device_status)
+        }
+        device_chk()//문서가 로딩되고 1번 실행
+        $(window).resize(function(){ //문서가 리사이즈될 때마다 1번씩 실행
+            device_chk()
+        })
 
         const swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
@@ -56,5 +70,24 @@ $(document).ready(function(){
             $('html, body').animate({
                 scrollTop : 0
             }, 500)
+        })
+
+        /***************biz over  ************** 
+         * .biz .list ul li에 마우스를 올리면 on 클래스 추가
+        .biz .list ul li에 마우스를 빼면 off클래스 추가 
+        **************************** */
+        $('.biz .list ul li').on('mouseenter', function(){
+            if(device_status == 'pc') {
+                $('.biz .list ul li').removeClass('on')
+                $('.biz .list ul li').addClass('off')
+                $(this).removeClass('off')
+                $(this).addClass('on')
+            }
+        })
+        $('.biz .list ul li').on('mouseleave', function(){
+            if(device_status == 'pc') {
+                $('.biz .list ul li').removeClass('on')
+                $('.biz .list ul li').removeClass('off')
+            }
         })
 })

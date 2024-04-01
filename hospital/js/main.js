8ca0595dd0db090a5883b60bc5cc73fb5d395b94
hyeinji0704 <157083486+hyeinji0704@users.noreply.github.com>
2024-04-01 14:28:32 +0900

    .

--- hospital/js/main.js
+++ hospital/js/main.js
@@ -131,4 +131,9 @@ $(document).ready(function(){
                 clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
             },
         });
+        AOS.init({
+            offset: 150, // 해당 콘텐츠가 하단에서 몇 px 위로 올라와에 나타나는 효과가 나타날지 셋팅하는 값
+            duration: 1000, // 애니메이션 효과가 작동되는 시간
+            easing: 'ease', // 가속도
+        });
 })

c3158d053b1bfa2d5f939fd8bf3bea35fd71951c
hyeinji0704 <157083486+hyeinji0704@users.noreply.github.com>
2024-04-07 23:30:41 +0900

    .

--- kps/js/main.js
+++ kps/js/main.js
@@ -5,14 +5,4 @@
     속  성 : 메인페이지에서 사용된 jqeury(header 빼고)
 */
 $(document).ready(function(){
-    window.addEventListener("load", () => {
-        let elBox = document.querySelector(".visual .tit .text03");
-        elBox.onclick = e => {
-            e.currentTarget.classList.toggle("logo_move");  
-        };
-        
-        elBox.onanimationend = e => {
-            $('.visual .logo_ani').attr('style', 'opacity: 1')
-        };
-    });
 })
